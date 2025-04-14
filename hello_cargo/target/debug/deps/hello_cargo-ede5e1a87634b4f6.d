41d21a1cce022b61b01b9ecb7e5164f3c29fb75c
yash <yashvikram8250@gmail.com>
2025-04-14 12:22:05 +0530

    cargo basics

---
+++ hello_cargo/target/debug/deps/hello_cargo-ede5e1a87634b4f6.d
@@ -0,0 +1,5 @@
+/home/yash-vikram/Desktop/the_rust_book_exercises/hello_cargo/target/debug/deps/libhello_cargo-ede5e1a87634b4f6.rmeta: src/main.rs
+
+/home/yash-vikram/Desktop/the_rust_book_exercises/hello_cargo/target/debug/deps/hello_cargo-ede5e1a87634b4f6.d: src/main.rs
+
+src/main.rs:
